e3158ed9f1943188270663d6ea0773dce113c15d
Ann <niyta@mail.ru>
2023-04-07 23:10:06 +0500

    Initial commit

---
+++ src/main/java/sky/pro/friendshiphouse/FriendshipHouseApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.friendshiphouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendshipHouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FriendshipHouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sky/pro/friendshiphouse/FriendshipHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.friendshiphouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendshipHouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
